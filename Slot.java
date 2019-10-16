d436d14ac883d352eccd5fae5d2fe44c3851362d
danish1371 <55708537+danish1371@users.noreply.github.com>
2019-10-16 15:04:43 -0500

    updated

--- Slot.java
+++ Slot.java
@@ -2,7 +2,8 @@
  * @author Danish Butt
  * 
  * This class is used to create a Slot type. A slot is considered to be one piece on the board.
- *  The xPos and yPos variables are used to set/get the location of slot within the 2-D array.
+ * The xPos and yPos variables are used for the location of slot within the 2-D array. 
+ * Example: [xPos][yPos] 
  */
 
 public class Slot {
@@ -11,20 +12,30 @@ public class Slot {
 	private int yPos;
 	private String name;
 
-	// General Constructor
+	/**
+	 * General Constructor for Slot class
+	 */
 	public Slot(int xPos, int yPos) {
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.name = "SS";
 	}
 
-	// Setter
+	/**
+	 * This is a setter which is used to set the initial values of x and y.
+	 * @param x This corresponds to the first value [x][]
+	 * @param y This corresponds to the second value [] [y] 
+	 * @return void
+	 */
 	public void setPos(int x, int y) {
 		this.xPos = x;
 		this.yPos = y;
 	}
 
-	// Getters
+	
+	/**
+	 * Getters
+	 */
 	public int getX() {
 		return this.xPos;
 	}
@@ -33,10 +44,16 @@ public class Slot {
 		return this.yPos;
 	}
 
+	/**
+	 * This is a setter which is used to set the name of the object
+	 * @param name Name of the object (Ex. WR corresponds to white rabbit)
+	 * @return void
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	
 	public String toString() {
 		return this.name;
 	}
