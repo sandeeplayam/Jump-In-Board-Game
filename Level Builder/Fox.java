853db4d4cf09190cc26318b03a688b2644ee3803
Tharsan <tharsan18@hotmail.ca>
2019-11-28 17:16:16 -0500

    milestone 3 files

---
+++ Level Builder/Fox.java
@@ -0,0 +1,399 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ * The Fox class of java game based on the children's game "JumpIn". This class
+ * contains the constructor which creates a instance of a fox. The class also
+ * has methods which can be invoked on the fox instance. It extends the Slot
+ * class and gets the variables and methods from that class
+ * 
+ * Milestone 1 author: Sandeep; Milestone 2 author: Tharsan, Omar
+ * 
+ * @author Sudarsana Sandeep
+ * @author Tharsan Sivathasan
+ * @author Omar Elberougy
+ *
+ */
+public class Fox extends Slot implements MovingPiece {
+
+	private int xPos2; // stores the coordinate of the tail x position
+	private int yPos2;// stores the coordinate of the tail y position
+	private boolean isVertical; // stores a boolean on whether the fox is vertical on the board
+	private Color color;
+	private ActionStorage moves;
+
+	/**
+	 * Constructor of Fox class that initializes its variables. Fox is made of two
+	 * pieces, the tail and the head which takes up two spaces on the board
+	 * 
+	 * @param xPos  Fox head x coordinate
+	 * @param yPos  Fox head y coordinate
+	 * @param xPos2 Fox tail x coordinate
+	 * @param yPos2 Fox tail y coordinate
+	 */
+	public Fox(int xPos, int yPos, int xPos2, int yPos2, Color c) {
+		super(xPos, yPos);
+		this.xPos2 = xPos2;
+		this.yPos2 = yPos2;
+		this.color = c;
+		moves = new ActionStorage();
+
+		if (yPos == yPos2) { // checking if fox is vertical
+			isVertical = true;
+		} else {
+			isVertical = false;
+		}
+	}
+
+	/**
+	 * Method returns if the Fox is vertical (facing up or down)
+	 * 
+	 * @return boolean true if the fox is vertically positioned on the board,
+	 *         otherwise false
+	 */
+	public boolean getVertical() {
+		return this.isVertical;
+	}
+
+	/**
+	 * Sets the x and y coordinates for the fox (head and tail coordinates)
+	 * 
+	 * @param x  Fox head x coordinate
+	 * @param y  Fox head y coordinate
+	 * @param x2 Fox tail x coordinate
+	 * @param y2 Fox tail y coordinate
+	 */
+	public void setPos(int x, int y, int x2, int y2) {
+		super.setPos(x, y); // setting both foxes head pieces
+		this.xPos2 = x2; // setting first fox tail piece
+		this.yPos2 = y2; // setting second fox tail piece
+	}
+
+	/**
+	 * Method gets the location of where the tail's x coordinate is located on the
+	 * board
+	 * 
+	 * @return Int the x coordinate of the fox's tail
+	 */
+	public int getTailX() {
+		return xPos2;
+	}
+
+	/**
+	 * Method gets the location of where the tail's y coordinate is located on the
+	 * board
+	 * 
+	 * @return Int the y coordinate of the fox's tail
+	 */
+	public int getTailY() {
+		return yPos2;
+	}
+
+	public boolean forward(int direction) {
+		boolean d = false;
+		switch (direction) {
+		case 1:// up
+
+			if (this.getX() < this.getTailX()) {
+				d = true;
+			}
+			break;
+		case 2:// down
+			if (this.getX() > this.getTailX()) {
+				d = true;
+			}
+			break;
+		case 3:// right
+
+			if (this.getY() > this.getTailY()) {
+				d = true;
+			}
+			break;
+		case 4:// left
+			if (this.getY() < this.getTailY()) {
+				d = true;
+			}
+			break;
+
+		}
+
+		return d;
+	}
+
+	/**
+	 * Calculates maximum number of spaces a fox can slide in a given direction and
+	 * returns that value
+	 * 
+	 * @param board     2d array filled with slot objects
+	 * @param row       the current x location of the fox
+	 * @param col       the current y location of the fox
+	 * @param direction 1 = up, 2, down, 3, right, 4 = left
+	 * @param spaces    starting spaces jumped (should always be 0)
+	 * @return spaces a fox can slide in a given direction
+	 */
+	public int canSlide(Slot[][] board, int row, int col, int direction, int spaces) {
+		// if slot being checked is out of the board
+		if (row < 0 || col < 0 || row >= board.length || col >= board.length) {
+			return spaces - 1;
+			// Recursively check how many slots are available (ignoring when spaces =0
+			// because thats when its the current fox)
+		} else if (spaces == 0 || board[row][col].getClass() == Slot.class) {
+
+			// check next slot as per direction
+			switch (direction) {
+			case 1: // up
+				return canSlide(board, row - 1, col, 1, spaces + 1);
+			case 2: // down
+				return canSlide(board, row + 1, col, 2, spaces + 1);
+			case 3: // right
+				return canSlide(board, row, col + 1, 3, spaces + 1);
+			case 4: // left
+				return canSlide(board, row, col - 1, 4, spaces + 1);
+			}
+
+		}
+		// stop recursion when no more slots available
+		return spaces - 1; // minus 1 because current location cannot be moved to
+	}
+
+	/**
+	 * Tries to move a fox to the inputed x and y coordinate in the board 2d array
+	 * inputed, and returns a boolean on whether or not the move was successful
+	 * 
+	 * @param board 2d array filled with slot objects
+	 * @param xPos  the desired x location to move the fox
+	 * @param yPos  the desired x location to move the fox
+	 * @return boolean whether a move was performed
+	 */
+	public boolean move(Slot[][] board, int xPos, int yPos, int undo) {
+
+		// check if the destination is within the row/column and coordinate is in the
+		// board
+		if (xPos < 0 || yPos < 0 || xPos >= board.length || yPos >= board.length) {
+			return false;
+		} else if (this.getX() != xPos && this.getY() != yPos) {
+			return false;
+		}
+
+		int spaces = -1; // Variable to store how many slots slid over
+		int xPos2 = -1;
+		int yPos2 = -1;
+
+		int leadX = this.getX(), leadY = this.getY(); // coordinate that is closest to direction that is being moved
+		int followX = this.getTailX(), followY = this.getTailY();
+
+		if (this.getY() == yPos) { // if moves up or down
+			if (!isVertical) {
+				return false;
+			}
+			if (this.getX() > xPos) { // if moves up
+				if (getTailX() == getX() - 1) { // looking down
+					leadX = getTailX();
+					followX = getX();
+				} else { // looking up
+					leadX = getX();
+					followX = getTailX();
+				}
+				spaces = -canSlide(board, leadX, this.getY(), 1, 0);
+			} else if (this.getX() < xPos) { // if moves down
+				if (getTailX() == getX() - 1) { // looking down
+					leadX = getX();
+					followX = getTailX();
+				} else { // looking up
+					leadX = getTailX();
+					followX = getX();
+				}
+				spaces = canSlide(board, leadX, this.getY(), 2, 0);
+			}
+			// If the players desired destination is within the movable distance
+			if (Math.abs(xPos - leadX) > Math.abs(spaces)) {
+				return false;
+			}
+
+//			xPos2 = followX + spaces;
+			xPos2 = followX + (xPos - leadX);
+			yPos2 = this.getTailY();
+
+		} else { // if moves right or left
+			if (isVertical) {
+				return false;
+			}
+			if (this.getY() > yPos) { // if moves left
+				if (this.getTailY() == getY() - 1) { // looking right
+					leadY = getTailY();
+					followY = getY();
+				} else { // looking left
+					leadY = getY();
+					followY = getTailY();
+				}
+				spaces = -canSlide(board, this.getX(), leadY, 4, 0);
+			} else if (this.getY() < yPos) { // if moves right
+				if (this.getTailY() == getY() - 1) { // looking right
+					leadY = getY();
+					followY = getTailY();
+				} else { // looking left
+					leadY = getTailY();
+					followY = getY();
+				}
+				spaces = canSlide(board, this.getX(), leadY, 3, 0);
+			}
+			// If the players desired coordinate matches the amount of spaces away the
+			// rabbit can move
+			if (Math.abs(yPos - leadY) > Math.abs(spaces)) {
+				return false;
+			}
+
+			// System.out.println("spaces "+spaces);
+
+			xPos2 = this.getTailX();
+//			yPos2 = followY + spaces;
+			yPos2 = followY + (yPos - leadY);
+		}
+
+		if (spaces == 0) { // If fox doesnt move
+			return false;
+		}
+
+		if (this.getY() > yPos || this.getX() > xPos) {// if going left or up
+			followY = leadY;// use lead as starting point for moving fox
+			followX = leadX;
+		}
+
+		if (undo > 0) {
+			moves.addMove(followX, followY, xPos, yPos, undo);
+		}
+
+		if (this.getX() > xPos && getTailX() == getX() - 1) { // move up looking down
+			int xTemp;
+			xTemp = xPos;
+			xPos = xPos2;
+			xPos2 = xTemp;
+		} else if (this.getY() > yPos && this.getTailY() == getY() - 1) { // move left looking right
+			int yTemp;
+			yTemp = yPos;
+			yPos = yPos2;
+			yPos2 = yTemp;
+		} else if (this.getX() < xPos && getTailX() == getX() + 1) { // move down looking up
+
+			int xTemp;
+			xTemp = xPos;
+			xPos = xPos2;
+			xPos2 = xTemp;
+		} else if (this.getY() < yPos && this.getTailY() == getY() + 1) { // move right looking left
+
+			int yTemp;
+			yTemp = yPos;
+			yPos = yPos2;
+			yPos2 = yTemp;
+		}
+
+		// Change current fox position on board to slot
+		board[this.getX()][this.getY()] = new Slot(this.getX(), this.getY());
+		board[this.getTailX()][this.getTailY()] = new Slot(this.getTailX(), this.getTailY());
+
+		// Change destination in board to fox
+		board[xPos][yPos] = this;
+		board[xPos2][yPos2] = this;
+
+		this.setPos(xPos, yPos, xPos2, yPos2);
+
+		return true;
+	}
+
+	@Override
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	public void redo(Slot[][] b) {
+		int index = moves.getRedoy().size() - 1;
+
+		if (this.move(b, moves.getundoX(index), moves.getundoY(index), 2)) {
+			moves.removeUndo();
+		}
+
+	}
+
+	public boolean undo(Slot[][] b) {
+		int numMoves = moves.getNumMoves();
+		boolean success = false;
+
+		int extrax = moves.getX(numMoves) - moves.getX(numMoves - 1);
+		int extray = moves.getY(moves.getNumMoves()) - moves.getY(moves.getNumMoves() - 1);
+
+		if (extray == 0) {
+			if (moves.getX(numMoves) > moves.getX(numMoves - 1)) {
+				success = this.move(b, moves.getX(numMoves - 1), moves.getY(numMoves - 1), 0);
+
+			} else if (moves.getX(numMoves) < moves.getX(numMoves - 1)) {
+				success = this.move(b, moves.getX(numMoves - 1) + 1, moves.getY(numMoves - 1), 0);
+
+			}
+
+		} else if (extrax == 0) {
+			if (moves.getY(numMoves) > moves.getY(numMoves - 1)) {
+				success = this.move(b, moves.getX(numMoves - 1), moves.getY(numMoves - 1), 0);
+
+			} else if (moves.getY(numMoves) < moves.getY(numMoves - 1)) {
+				success = this.move(b, moves.getX(numMoves - 1), moves.getY(numMoves - 1) + 1, 0);
+
+			}
+		}
+
+		// System.out.println("succesundo "+success);
+		if (success) {
+
+			moves.addUndoMove();
+		}
+		return success;
+
+	}
+
+	public boolean canUndo() {
+		return (!moves.getMovex().isEmpty());
+	}
+
+	public ArrayList<Integer> possibleMoves(Board board) {
+
+		int dir = 1;
+		ArrayList<Integer> foxMoves = new ArrayList<Integer>();
+
+		while (dir < 5) {
+
+			int spaces = 0;
+
+			if (this.forward(dir)) {
+
+				spaces = (this.canSlide(board.getBoard(), this.getX(), this.getY(), dir, 0));// uses head as lead
+
+			} else {// use tail
+				spaces = (this.canSlide(board.getBoard(), this.getTailX(), this.getTailY(), dir, 0));// uses tail as the
+																										// lead
+			}
+
+			if (((dir < 3) && this.getVertical()) || ((dir > 2) && (!this.getVertical()))) {// If
+																							// going
+																							// proper
+																							// direction
+
+				if (spaces > 0) {
+					foxMoves.add(dir);
+					foxMoves.add(spaces);
+				}
+			}
+
+			dir++;
+		}
+
+		return foxMoves;
+	}
+
+	@Override
+	public void clearMoves() {
+		this.moves.clearMoves();
+	}
+}
---
+++ Level Builder/View.java
@@ -0,0 +1,336 @@
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+//import java.awt.BorderLayout;
+//import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.GridBagConstraints;
+
+/**
+ * This class is what controls the View and controller portion of the MVC for
+ * the game JumpIn'. This class gets the data of the pieces from the board class
+ * and shows a gui. it also performs actions (the controller portion of the MVC)
+ * when a button is clicked. The View class calls the methods in this class to
+ * update the GUI.
+ * 
+ * @author David Ou
+ * @author Sudarsana Sandeep
+ * @author Tharsan Sivathasan
+ * @author Omar Elberougy
+ *
+ */
+public class View {
+
+	private JFrame frame;
+	private JMenuItem moveItem, undoItem, redoItem, hintItem;
+	private Controller controller;
+
+	public JFrame getFrame() {
+		return this.frame;
+	}
+
+	/**
+	 * Creates instance of the view class allowing the GUI to be shown
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try { // Catches exceptions that may be thrown
+					View window = new View();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Constructor for the class that initializes the frame and creates a menubar
+	 */
+	public View() {
+		controller = new Controller(this);
+		// initialize the beginning and end coordinates to -1 (which is not found on the
+		// board) so we know if the value changes
+
+		frame = new JFrame("Jump In'");// Set name of frame
+		// Set icon of frame to a rabbit image
+		ImageIcon logoImage = new ImageIcon(getClass().getResource("Wrabbit.png"));
+		frame.setIconImage(logoImage.getImage());
+		frame.setSize(515, 560);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
+
+		JPanel gamePanel = new JPanel(); // panel that stores the card layout
+		frame.setContentPane(gamePanel);
+
+		// Creates menu bar that houses options for rules, quit, and return to main menu
+		JMenuBar menuBar = new JMenuBar();
+		JMenu optionMenu = new JMenu("Options");
+		JMenuItem rules = new JMenuItem("Rules");
+		JMenuItem quit = new JMenuItem("Quit");
+		JMenuItem returnMain = new JMenuItem("Return to Main Menu");
+		frame.setJMenuBar(menuBar);
+		menuBar.add(optionMenu);
+		optionMenu.add(rules);
+		optionMenu.add(quit);
+		optionMenu.add(returnMain);
+
+		menuBar.add(Box.createHorizontalGlue());
+		// creates move button that is not visible until you see the board
+		hintItem = new JMenuItem("Hint");
+		frame.getJMenuBar().add(hintItem);
+		hintItem.addActionListener(controller);
+		hintItem.setVisible(false);
+		// creates move button that is not visible until you see the board
+		undoItem = new JMenuItem("Undo");
+//		frame.getJMenuBar().add(Box.createHorizontalGlue());
+		frame.getJMenuBar().add(undoItem);
+		undoItem.addActionListener(controller);
+		undoItem.setVisible(false);
+		// creates move button that is not visible until you see the board
+		redoItem = new JMenuItem("Redo");
+//		frame.getJMenuBar().add(Box.createHorizontalGlue());
+		frame.getJMenuBar().add(redoItem);
+		redoItem.addActionListener(controller);
+		redoItem.setVisible(false);
+		// creates move button that is not visible until you see the board
+		moveItem = new JMenuItem("Move");
+//		frame.getJMenuBar().add(Box.createHorizontalGlue());
+		frame.getJMenuBar().add(moveItem);
+		moveItem.addActionListener(controller);
+		moveItem.setVisible(false);
+
+		rules.addActionListener(controller);
+		quit.addActionListener(controller);
+		returnMain.addActionListener(controller);
+
+		startMenu(); // Calls method that creates a card displays that card on the frame
+
+	}
+
+	/**
+	 * Method that creates a panel of the start screen and adds it to the list of
+	 * panels in card layout which can be cycled through
+	 */
+	public void startMenu() {
+
+		moveItem.setVisible(false);
+		redoItem.setVisible(false);
+		undoItem.setVisible(false);
+		hintItem.setVisible(false);
+
+		frame.getContentPane().removeAll();
+		JPanel startMenu = new JPanel(new BorderLayout());
+
+		// Shows the logo on the start screen
+		ImageIcon logoImage = new ImageIcon(getClass().getResource("Jump In Logo.jpg"));
+		JLabel logo = new JLabel(logoImage);
+		logo.setPreferredSize(new Dimension(300, 300));
+		startMenu.add(logo, BorderLayout.NORTH);
+
+		// Create a button and adds it to the startmenu screen
+		JButton newGame = new JButton("New Game");
+		newGame.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		startMenu.add(newGame, BorderLayout.SOUTH);
+
+		newGame.addActionListener(controller);
+
+		frame.add(startMenu);// Adds the start menu to the JFrame
+		frame.validate();
+		frame.repaint();
+	}
+
+	/**
+	 * Method that creates a panel of the level select screen and adds it to the
+	 * list of panels in card layout which can be cycled through
+	 */
+	public void levelSelect() {
+
+		frame.getContentPane().removeAll();
+		JPanel levelSelect = new JPanel(new BorderLayout());
+		frame.getContentPane().add(levelSelect);
+
+		// Shows the logo on the level select screen
+		ImageIcon logoImage = new ImageIcon(getClass().getResource("Jump In Logo.jpg"));
+		JLabel logo = new JLabel(logoImage);
+		logo.setPreferredSize(new Dimension(300, 300));
+		levelSelect.add(logo, BorderLayout.NORTH);
+
+		JPanel levelButtons = new JPanel(new GridLayout(2,2));
+
+		// Creates 5 level buttons to be placed side by side
+		JButton level1 = new JButton("Level 1");
+		level1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		levelButtons.add(level1);
+		JButton level2 = new JButton("Level 2");
+		level2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		levelButtons.add(level2);
+		JButton level3 = new JButton("Level 3");
+		level3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		levelButtons.add(level3);
+		JButton level4 = new JButton("Level 4");
+		level4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		levelButtons.add(level4);
+
+		levelSelect.add(levelButtons, BorderLayout.CENTER);
+
+		// Creates a start button that is placed below the level buttons
+		JButton start = new JButton("Start");
+		start.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		levelSelect.add(start, BorderLayout.SOUTH);
+
+		level1.addActionListener(controller);
+		level2.addActionListener(controller);
+		level3.addActionListener(controller);
+		level4.addActionListener(controller);
+		start.addActionListener(controller);
+
+		frame.validate();
+		frame.repaint();
+
+	}
+
+	/**
+	 * This method creates a panel for when the board gui is showing. Uses a
+	 * gridbaglayout to add pieces on the board's 2d array
+	 */
+	public void startLevel(Board b) {
+
+		moveItem.setVisible(true);
+		redoItem.setVisible(true);
+		undoItem.setVisible(true);
+		hintItem.setVisible(true);
+
+		JPanel startLevel = new JPanel();
+		GridBagConstraints gbc = new GridBagConstraints();
+		Slot[][] tempBoard = b.getBoard();
+		Dimension square = new Dimension(100, 100); // size of each button in the grid (except fox)
+
+		startLevel.setLayout(new GridBagLayout());
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(startLevel);
+
+		// Iterates over the board's 2d array
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+
+				JButton tempButton;
+				ImageIcon imageIcon;
+				Image image;
+
+				String fileName = "";
+				// get class to determine if its a rabbit fox mushroom hole or slot
+				Class<? extends Slot> tempClass = tempBoard[x][y].getClass();
+
+				// set row, column and size of each square
+				gbc.gridx = y; // set row number to the row number of the 2d array
+				gbc.gridy = x; // set column number to the column number of the 2d array
+				gbc.gridheight = 1; // set height of the column to 1
+				gbc.gridwidth = 1; // set width of the column to 1
+
+				if (tempClass == Rabbit.class) { // if the object is instance of rabbit
+					// get color from the rabbit and pick the correct file based on color
+					Color rabbitColour = ((Rabbit) tempBoard[x][y]).getColor();
+					if (rabbitColour == Color.WHITE) {
+						fileName = "Wrabbit.png"; // change to white rabbit image file
+					} else if (rabbitColour == Color.ORANGE) {
+						fileName = "Orabbit.png"; // change to orange rabbit image file
+					} else if (rabbitColour == Color.GRAY) {
+						fileName = "Grabbit.png"; // change to grey rabbit image file
+					}
+				} else if (tempClass == Fox.class) {// if the object is instance of fox
+					Fox tempFox = (Fox) tempBoard[x][y]; // store the fox object temporarily
+
+					if (tempFox.getX() == x && tempFox.getY() == y) { // if the head of the fox is the current position
+						if (tempFox.getVertical()) { // if facing up or down
+							if (tempFox.getTailX() == x - 1) {// if facing up
+								fileName = "Fox Hdown.png"; // change to vertical head down fox file
+								gbc.gridy = x - 1;
+							} else { // if facing down
+								fileName = "Fox Hup.png"; // change to vertical head up fox file
+							}
+							gbc.gridheight = 2; // overriding height dimension to fox length
+
+						} else { // if facing right or left
+							if (tempFox.getTailY() == y - 1) { // if facing left
+								gbc.gridx = y - 1;
+								fileName = "Fox Hright.png"; // change to horizontal head left fox file
+
+							} else {// if facing right
+								fileName = "Fox Hleft.png"; // change to horizontal head right fox file
+							}
+							// buttonName = "HFox";
+							gbc.gridwidth = 2; // overriding width dimension
+						}
+					} else {// if the tail of the fox is the current position do nothing
+						continue; // waits until the the fox head comes
+					}
+				} else if (tempClass == Mushroom.class) {// if the object is instance of Mushroom
+					fileName = "mushroom image.png";
+
+				} else if (tempClass == Hole.class) {// if the object is instance of Hole
+					if (((Hole) tempBoard[x][y]).hasGamePiece()) { // if the hole has a gamepiece (rabbit or mushroom)
+						if (((Hole) tempBoard[x][y]).hasRabbit()) {// if the hole has a rabbit inside
+							if (((Rabbit) ((Hole) tempBoard[x][y]).getGamePiece()).getColor() == Color.WHITE) {
+								fileName = "Whole.png";// set picture to a white rabbit in a hole
+
+							} else if (((Rabbit) ((Hole) tempBoard[x][y]).getGamePiece()).getColor() == Color.ORANGE) {
+								fileName = "Ohole.png";// set picture to a orange rabbit in a hole
+
+							} else if (((Rabbit) ((Hole) tempBoard[x][y]).getGamePiece()).getColor() == Color.GRAY) {
+								fileName = "Ghole.png";// set picture to a Gray rabbit in a hole
+
+							}
+						} else { // if the hole has a mushroom inside
+							fileName = "mushroom image.png"; // set picture to a mushroom
+						}
+					} else { // if the hole doesnt have game piece
+						fileName = "hole.png"; // set picture to a hole
+					}
+				} else { // if the object is an instance of a slot
+					fileName = "empty slot.png";
+				}
+
+				// Initialize the button with the filename and location set when determining
+				// which instance of a class it was
+				tempButton = new JButton();
+				imageIcon = new ImageIcon(getClass().getResource(fileName));
+				image = imageIcon.getImage();
+				// set size of the image to 100 by 100 (same size as the button)
+				image = image.getScaledInstance(gbc.gridwidth * 100, gbc.gridheight * 100, Image.SCALE_SMOOTH);
+				imageIcon = new ImageIcon(image);
+
+				tempButton.setIcon(imageIcon); // set picture in icon to image
+				tempButton.setSize(square); // set dimensions of button
+				tempButton.setFocusPainted(false);
+				tempButton.addActionListener(controller);
+				tempButton.setBorder(BorderFactory.createEmptyBorder());
+
+				startLevel.add(tempButton, gbc);
+			}
+		}
+		// set the grid bag constraints to the last column and row so the board's gui
+		// alignment is northwest in the frame
+		gbc.gridx = 5;
+		gbc.gridy = 5;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		JLabel label = new JLabel();
+		startLevel.add(label, gbc);
+
+		frame.validate();
+		frame.repaint();
+
+	}
+}
